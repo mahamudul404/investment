2c40d4cf2542ac5ead94230cf9c2ef796a312253
Your New Name <mahamudul408432@gmail.com>
2024-11-16 09:47:56 +0600

    manage auth

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,13 @@
+<div>
+    <label for="phone">Phone</label>
+    <input id="phone" type="tel" name="phone" required autofocus>
+</div>
+<div>
+    <label for="password">Password</label>
+    <input id="password" type="password" name="password" required>
+</div>
+<div>
+    <label for="password_confirmation">Confirm Password</label>
+    <input id="password_confirmation" type="password" name="password_confirmation" required>
+</div>
+<button type="submit">Register</button>
